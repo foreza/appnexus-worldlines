d2cc2d64f5b4da6d2aa6922cedcf2b116984616a
Jason C <jforeza@gmail.com>
2018-06-28 16:49:26 -0700

    Update project to use sample test placement
    Several of the swift methods that they were using are no longer functional, provided a working version here
    None of the ANDelegate methods had an example in swift, I'll have to hunt those down from the source
    :

--- worldlines/FirstViewController.swift
+++ worldlines/FirstViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import AppNexusSDK
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, ANAdDelegate  {
+    
+    var logger : String = "[WURLLDLINES]"
+    var banner : ANBannerAdView?;
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let screenRect = UIScreen.main.bounds
+        let centerX = screenRect.size.width/2
+        let centerY = screenRect.size.height/2
+        let rect = CGRect(x: CGFloat(centerX), y: CGFloat(centerY), width: CGFloat(300.0), height: CGFloat(250.0));
+        let size = CGSize(width: 300.0, height: 250.0)
+        
+        let banner = ANBannerAdView (frame: rect, placementId:"1281482", adSize:size);
+
+    
+        // Create the banner ad view and add it as a subview.
+        banner?.rootViewController = self;
+        banner?.autoRefreshInterval = 30   // Set to 0 to disable auto-refresh.
+        self.view.addSubview(banner!)
+        
+        // Load an ad!
+        banner?.loadAd()
+        
+        print(logger + " loadAd called")
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // TODO: Figure out the other ANAdDelegate methods and implement them
+    func adDidReceiveAd(_ id: ANAdProtocol!) {
+        print(logger + " did received ad")
+    }
+    
 
 }
 
